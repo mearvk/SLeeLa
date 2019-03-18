1e8dd6069627a4d58f8f4c2ec138efa5de730e5b
Mr. Max Rupplin <mearvk@outlook.com>
2019-03-18 13:41:00 -0400

    [ latest ]
    Signed-off-by: Mr. Max Rupplin <mearvk@outlook.com>

--- src/implementations/one/nordshrift/drivers/NordshriftDriver.java
+++ src/implementations/one/nordshrift/drivers/NordshriftDriver.java
@@ -2,19 +2,13 @@ package implementations.one.nordshrift.drivers;
 
 import implementations.one.bodi.Bodi;
 import implementations.one.nordshrift.events.RegisterableNordshriftEvent;
-import implementations.one.nordshrift.factory.NordshriftFactory;
 
 public class NordshriftDriver
 {
-    public NordshriftDocument document001 = new NordshriftDocument("implementations.one.documents/nordshrift.ns");
+    public NordshriftDocument document001 = new NordshriftDocument("implementations/one/documents/nordshrift.ns");
 
     public NordshriftDriver()
     {
         Bodi.reference.push("::", new RegisterableNordshriftEvent());
-    }
-
-    public void factory()
-    {
-        NordshriftFactory factory = new NordshriftFactory();
     }
 }
