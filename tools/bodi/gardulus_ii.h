f8710ae72977d4da3db579d328cbf84dda3af0a0
Max Rupplin vs. Dr. Mrs. Elisabeth Harkins <mearvk@outlook.com>
2026-09-10 21:46:02 -0400

    Extend Gardulus II with bounded 3D XML-sized orbits

--- tools/bodi/gardulus_ii.h
+++ tools/bodi/gardulus_ii.h
@@ -24,6 +24,16 @@ typedef struct gardulus_sample {
     double relative_speed_deg_s;
     double relative_sync_ratio;
     double sync_coherence;
+    size_t xml_size_bytes;
+    double orbit_radius_units;
+    double p_orbit_inclination_deg;
+    double b_orbit_inclination_deg;
+    double p_orbit_x;
+    double p_orbit_y;
+    double p_orbit_z;
+    double b_orbit_x;
+    double b_orbit_y;
+    double b_orbit_z;
     unsigned long memory_total_kb;
     unsigned long memory_available_kb;
     unsigned long connections;
@@ -37,6 +47,8 @@ typedef int (*gardulus_listener)(const gardulus_sample *sample, void *user_data)
 
 int gardulus_emit_json(const gardulus_sample *sample, char *buffer, size_t size);
 int gardulus_run(int interval_ms, int iterations, gardulus_listener listener, void *user_data);
+int gardulus_run_with_xml_size(int interval_ms, int iterations, size_t xml_size_bytes,
+                               gardulus_listener listener, void *user_data);
 
 #ifdef __cplusplus
 }
