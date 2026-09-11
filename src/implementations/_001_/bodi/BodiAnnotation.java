42d10bb94fb60506046fc8d593871d20152063e3
Max Rupplin vs. Dr. Mrs. Elisabeth Harkins <mearvk@outlook.com>
2026-09-10 21:09:07 -0400

    Make Bodi annotations runtime-discoverable

--- src/implementations/_001_/bodi/BodiAnnotation.java
+++ src/implementations/_001_/bodi/BodiAnnotation.java
@@ -1,12 +1,17 @@
 package implementations._001_.bodi;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/** Declarative metadata for Bodi-discoverable operations. */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD})
 public @interface BodiAnnotation
 {
-    public String protocol() default "";
-
-    public String classname() default "";
-
-    public String methodname() default "";
-
-    public String requirement() default "";
+    String protocol() default "";
+    String classname() default "";
+    String methodname() default "";
+    String requirement() default "";
 }
