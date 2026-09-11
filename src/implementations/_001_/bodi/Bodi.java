9228171df96a585285222bad99cbf7bfb75b360c
Max Rupplin vs. Dr. Mrs. Elisabeth Harkins <mearvk@outlook.com>
2026-09-10 21:07:30 -0400

    Make Bodi the preferred fluent witness layer

--- src/implementations/_001_/bodi/Bodi.java
+++ src/implementations/_001_/bodi/Bodi.java
@@ -4,107 +4,102 @@ import implementations._001_.nordshrift.events.RegisterableNordshriftEvent;
 import java.rmi.Remote;
 import java.rmi.registry.LocateRegistry;
 
+/**
+ * Bodi is the witness layer for change to an addressed object.
+ *
+ * Preferred form:
+ * Bodi.system("xxx").propagative("001").install("yyy");
+ *
+ * Java RMI remains the transport/registry compatibility layer underneath it.
+ */
 public class Bodi extends BodiExtent implements Remote
 {
     public static Bodi reference = new Bodi();
-
     public Bodi bodi = reference;
-
-    //
 
     static
     {
-        //System.out.println("  [ns::bodi]       Bodi Registry™ initialized");
-
         try
         {
-            BodiExtender.registry001 = LocateRegistry.createRegistry(8888);
-
-            BodiExtender.registry001.bind("@bodi", Bodi.reference);
+            BodiExtender.registry001 = LocateRegistry.createRegistry(BodiNetworkConfig.DEFAULT_RMI_PORT);
+            BodiExtender.registry001.rebind("@bodi", Bodi.reference);
         }
         catch (Exception exception)
         {
             java.lang.System.out.println(exception);
         }
+    }
+
+    public BodiObjectReference system(String name)
+    {
+        return this.extender001.system(name);
+    }
+
+    public BodiNetworkServer startNetwork(BodiNetworkConfig config) throws Exception
+    {
+        BodiNetworkServer server = new BodiNetworkServer(this.extender001);
+        if (config != null && config.enabled)
+            server.start(config.bodiPort);
+        return server;
     }
 
     public void run(Encapsulator encapsulator)
     {
-
+        // Witness execution is represented by the fluent API and invoke().
     }
 
     public void run(String protocol, String bodiref, String methodname)
     {
-
+        try
+        {
+            this.extender001.invoke(new BodiChange(bodiref, methodname, "", ""),
+                protocol, "run");
+        }
+        catch (Exception exception)
+        {
+            java.lang.System.out.println(exception);
+        }
     }
 
     public void run(String protocol, String bodiref, String methodname, StackTraceElement[] stack)
     {
-
+        run(protocol, bodiref, methodname);
     }
-
-    //
 
     public Remote pull(String bodiref)
     {
-        try
-        {
-            return this.extender001.pull(bodiref);
-        }
+        try { return this.extender001.pull(bodiref); }
         catch (Exception exception)
         {
             java.lang.System.out.println(exception);
+            return null;
         }
-
-        return null;
     }
 
     public void push(String bodiref, Remote remote)
     {
-        try
-        {
-            this.extender001.push(bodiref, remote);
-        }
-        catch (Exception exception)
-        {
-            java.lang.System.out.println(exception);
-        }
+        try { this.extender001.push(bodiref, remote); }
+        catch (Exception exception) { java.lang.System.out.println(exception); }
     }
 
     public void push(String bodiref, Class klass)
     {
         try
         {
-            //this.extender001.push(bodiref, remote);
+            Remote remote = (Remote) klass.getDeclaredConstructor().newInstance();
+            this.extender001.push(bodiref, remote);
         }
-        catch (Exception exception)
-        {
-            java.lang.System.out.println(exception);
-        }
+        catch (Exception exception) { java.lang.System.out.println(exception); }
     }
 
     public void push(String bodiref, Remote remote, RegisterableNordshriftEvent event)
     {
-        try
-        {
-            this.extender001.push(bodiref, remote);
-        }
-        catch (Exception exception)
-        {
-            java.lang.System.out.println(exception);
-        }
+        push(bodiref, remote);
     }
 
     public void push(String bodiref, Remote remote, Class<RegisterableNordshriftEvent> klass)
     {
-        try
-        {
-            this.extender001.push(bodiref, remote);
-        }
-        catch (Exception exception)
-        {
-            java.lang.System.out.println(exception);
-        }
+        push(bodiref, remote);
     }
 
     class Encapsulator
@@ -113,15 +108,13 @@ public class Bodi extends BodiExtent implements Remote
 
         public Encapsulator(StackTraceElement[] stacktrace)
         {
-            for(StackTraceElement element : stacktrace)
-            {
+            for (StackTraceElement element : stacktrace)
                 this.buffer.append(element);
-            }
         }
 
         public boolean contains(String string)
         {
-            return (this.buffer.toString().contains(string));
+            return this.buffer.toString().contains(string);
         }
     }
 }
