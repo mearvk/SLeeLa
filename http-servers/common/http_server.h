7d599a196ac4f5f17db916b5526b993cc8d5709f
Max Rupplin vs. Dr. Mrs. Elisabeth Harkins <mearvk@outlook.com>
2026-09-23 01:30:29 -0400

    Add native SLeeLa HTTP server grades

---
+++ http-servers/common/http_server.h
@@ -0,0 +1,10 @@
+#ifndef SLEELA_HTTP_SERVER_H
+#define SLEELA_HTTP_SERVER_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+int sleela_http_server_run(const char *version,int argc,char **argv);
+#ifdef __cplusplus
+}
+#endif
+#endif
