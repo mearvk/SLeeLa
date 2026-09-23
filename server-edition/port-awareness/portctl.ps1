2ea5cd680ec99d985f7e3f6557f34b48cfa63ea2
Max Rupplin vs. Dr. Mrs. Elisabeth Harkins <mearvk@outlook.com>
2026-09-22 22:22:05 -0400

    Add Server Edition port awareness: server-edition/port-awareness/portctl.ps1

---
+++ server-edition/port-awareness/portctl.ps1
@@ -0,0 +1,32 @@
+param(
+  [Parameter(Mandatory=$true)][ValidateSet("open","close","status")][string]$Action,
+  [Parameter(Mandatory=$true)][ValidateSet("Discord-1","Discord-2","Discord-3")][string]$Edition,
+  [Parameter(Mandatory=$true)][ValidateRange(1,65535)][int]$Port,
+  [ValidateSet("tcp","udp")][string]$Protocol = "tcp"
+)
+
+$ErrorActionPreference = "Stop"
+$RuleName = "SLeeLa-$Edition-$Protocol-$Port"
+$os = Get-CimInstance Win32_OperatingSystem
+$version = $os.Version
+
+function Remove-SleeLaRule {
+  Get-NetFirewallRule -DisplayName $RuleName -ErrorAction SilentlyContinue |
+    Remove-NetFirewallRule -ErrorAction SilentlyContinue
+}
+
+switch ($Action) {
+  "open" {
+    Remove-SleeLaRule
+    New-NetFirewallRule -DisplayName $RuleName -Direction Inbound -Action Allow -Protocol $Protocol -LocalPort $Port -Profile Any -Enabled True | Out-Null
+  }
+  "close" {
+    Remove-SleeLaRule
+  }
+  "status" {
+    Get-NetFirewallRule -DisplayName $RuleName -ErrorAction SilentlyContinue |
+      Get-NetFirewallPortFilter | Select-Object Protocol,LocalPort
+  }
+}
+
+Write-Output "SLeeLa portctl: action=$Action edition=$Edition port=$Port/$Protocol os=Windows version=$version firewall=Windows Defender Firewall rule=$RuleName"
