ed6d7b48d1a30d5091ecd90fccaac6103de67bb5
Max Rupplin vs. Dr. Mrs. Elisabeth Harkins <mearvk@outlook.com>
2026-09-23 01:34:49 -0400

    Add native HTTP/2 server transport

---
+++ http-servers/2/http2/http2_server.h
@@ -0,0 +1,10 @@
+#ifndef SLEELA_HTTP2_SERVER_H
+#define SLEELA_HTTP2_SERVER_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+int sleela_http2_server_run(int argc,char **argv);
+#ifdef __cplusplus
+}
+#endif
+#endif
