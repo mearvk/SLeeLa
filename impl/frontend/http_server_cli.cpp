97a5742341e2fd2538bad97c708d4045f835eda1
Max Rupplin vs. Dr. Mrs. Elisabeth Harkins <mearvk@outlook.com>
2026-09-23 01:37:50 -0400

    Wire SLeeLa HTTP/3 Grade 3 into server and CLI

--- impl/frontend/http_server_cli.cpp
+++ impl/frontend/http_server_cli.cpp
@@ -1,11 +1,23 @@
 #include "http_server_cli.h"
 #include "../../http-servers/common/http_server.h"
 #include "../../http-servers/2/http2/http2_server.h"
+#include "../../http-servers/3/http3/http3_server.h"
 #include <iostream>
 #include <string>
-int sleelaHttpServerCommand(int argc,char**argv){
-  if(argc<3){std::cerr<<"sleela http-server: expected grade 1, 2, or 3\n";return 2;}
-  std::string g=argv[2];
-  if(g!="1"&&g!="2"&&g!="3"){std::cerr<<"sleela http-server: grade must be 1, 2, or 3\n";return 2;}
-  if(g=="2") return sleela_http2_server_run(argc-2,argv+2);\n  return sleela_http_server_run(g.c_str(),argc-2,argv+2);
+
+int sleelaHttpServerCommand(int argc, char** argv) {
+  if (argc < 3) {
+    std::cerr << "sleela http-server: expected grade 1, 2, or 3\n";
+    return 2;
+  }
+
+  std::string g = argv[2];
+  if (g != "1" && g != "2" && g != "3") {
+    std::cerr << "sleela http-server: grade must be 1, 2, or 3\n";
+    return 2;
+  }
+
+  if (g == "2") return sleela_http2_server_run(argc - 2, argv + 2);
+  if (g == "3") return sleela_http3_server_run(argc - 2, argv + 2);
+  return sleela_http_server_run(g.c_str(), argc - 2, argv + 2);
 }
