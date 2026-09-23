055dd6cb3b1398559a8c1737b91f90c3fa3016aa
Max Rupplin vs. Dr. Mrs. Elisabeth Harkins <mearvk@outlook.com>
2026-09-23 01:37:08 -0400

    Add SLeeLa HTTP/3 Grade 3 transport adapter

---
+++ http-servers/3/http3/http3_server.h
@@ -0,0 +1,14 @@
+#ifndef SLEELA_HTTP3_SERVER_H
+#define SLEELA_HTTP3_SERVER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int sleela_http3_server_run(int argc, char **argv);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
