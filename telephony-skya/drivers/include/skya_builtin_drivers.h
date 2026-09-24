ccb889b265444fe128e0f41c573beb44978785f0
Max Rupplin vs. Dr. Mrs. Elisabeth Harkins <mearvk@outlook.com>
2026-09-24 18:15:42 -0400

    Add Skya built-in driver declarations

---
+++ telephony-skya/drivers/include/skya_builtin_drivers.h
@@ -0,0 +1,10 @@
+#ifndef SKYA_BUILTIN_DRIVERS_H
+#define SKYA_BUILTIN_DRIVERS_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+int skya_register_builtin_drivers(void);
+#ifdef __cplusplus
+}
+#endif
+#endif
