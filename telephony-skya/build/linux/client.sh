ca282f287b55f1533fb71ac2dfad9a43e791331b
Max Rupplin vs. Dr. Mrs. Elisabeth Harkins <mearvk@outlook.com>
2026-09-23 23:16:30 -0400

    Separate native Linux client launcher

--- telephony-skya/build/linux/client.sh
+++ telephony-skya/build/linux/client.sh
@@ -2,6 +2,4 @@
 set -eu
 ROOT="$(cd "$(dirname "$0")/../.." && pwd)"
 "$ROOT/build/linux/build.sh"
-export SKYA_SLEEELA_CIRCUIT="$ROOT/sleela/SkyaClient.sleela"
-cd "$ROOT/javafx"
-exec mvn -q javafx:run
+exec "$ROOT/build/linux/skya" --client
