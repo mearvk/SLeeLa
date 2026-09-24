55753c34b7ab42fda97e1ba7cbb9e8f9f7f8a4ab
Max Rupplin vs. Dr. Mrs. Elisabeth Harkins <mearvk@outlook.com>
2026-09-24 00:43:37 -0400

    Add Linux firewall preflight to remote client

--- telephony-skya/build/linux/remote-client.sh
+++ telephony-skya/build/linux/remote-client.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -eu
 ROOT="$(cd "$(dirname "$0")/../.." && pwd)"
+"$ROOT/build/linux/firewall-check.sh"
 cd "$ROOT/javafx"
 exec mvn -q -DmainClass=com.mearvk.sleela.skya.SkyaConnectApp javafx:run
