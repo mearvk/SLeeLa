128421849932bd7629341af2a360b0591a7dda0f
Max Rupplin vs. Dr. Mrs. Elisabeth Harkins <mearvk@outlook.com>
2026-09-24 23:07:10 -0400

    Register remaining model drivers before vendor fallbacks

--- telephony-skya/drivers/src/skya_builtin_drivers.cpp
+++ telephony-skya/drivers/src/skya_builtin_drivers.cpp
@@ -1,6 +1,7 @@
 #include "../include/skya_phone_driver.h"
 #include "../include/skya_model_drivers.h"
 #include "../include/skya_vendor_drivers.h"
+#include "../include/skya_remaining_model_drivers.h"
 #include "../src/skya_driver_registry.h"
 
 static int standard_audio_probe(const skya_driver_device *d) {
@@ -18,6 +19,40 @@ static const skya_phone_driver standard_audio_driver={
 
 extern "C" int skya_register_builtin_drivers(void) {
     const skya_phone_driver *drivers[]={
+        skya_yealink_uh42_driver(),
+        skya_yealink_uh44_driver(),
+        skya_yealink_wh64_driver(),
+        skya_yealink_wh68_driver(),
+        skya_yealink_uh46_driver(),
+        skya_poly_blackwire_3320_driver(),
+        skya_poly_savi_8200_driver(),
+        skya_poly_savi_8400_driver(),
+        skya_poly_voyager_4320_driver(),
+        skya_poly_voyager_5200_driver(),
+        skya_jabra_biz1500_driver(),
+        skya_jabra_evolve3_65_driver(),
+        skya_jabra_evolve3_75_driver(),
+        skya_jabra_speak2_55_driver(),
+        skya_jabra_speak2_75_driver(),
+        skya_grandstream_guv3005_driver(),
+        skya_grandstream_gxp21xx_driver(),
+        skya_grandstream_grp26xx_driver(),
+        skya_epos_impact_sc200_driver(),
+        skya_epos_impact_1000_driver(),
+        skya_epos_sdw5000_driver(),
+        skya_logitech_zone_wireless2_driver(),
+        skya_logitech_zone_vibe_driver(),
+        skya_logitech_h570e_driver(),
+        skya_logitech_zone305_driver(),
+        skya_fanvil_x5uv2_driver(),
+        skya_fanvil_x6uv2_driver(),
+        skya_fanvil_v63_driver(),
+        skya_fanvil_v64_driver(),
+        skya_fanvil_v65_driver(),
+        skya_fanvil_v66_driver(),
+        skya_fanvil_x210iv2_driver(),
+        skya_snom_a330m_driver(),
+        skya_cisco_322_driver(),
         skya_yealink_mp45_driver(), skya_yealink_mp50_driver(),
         skya_poly_blackwire_5220_driver(), skya_jabra_evolve2_40_driver(),
         skya_grandstream_guv3000_driver(), skya_epos_impact_sc600_driver(),
