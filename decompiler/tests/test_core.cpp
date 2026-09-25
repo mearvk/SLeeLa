34560a46c9f48a30e2d36c36bc847b6d1fd63737
Max Rupplin vs. Dr. Mrs. Elisabeth Harkins <mearvk@outlook.com>
2026-09-25 00:39:26 -0400

    Add Slecompiler Stage 3 archive coverage

--- decompiler/tests/test_core.cpp
+++ decompiler/tests/test_core.cpp
@@ -1,11 +1,33 @@
 #include "sleela/decompiler/decompiler.hpp"
 #include <cassert>
+#include <cstdint>
+#include <string>
+#include <vector>
+
 int main() {
-    const unsigned char bytes[] = {'M','Z',0,1,2,3};
-    auto a = sleela::decompiler::Artifact::from_bytes(bytes);
-    assert(a.format() == sleela::decompiler::Format::PE);
-    assert(a.size() == 6);
-    auto i = sleela::decompiler::Decoder{}.decode(a, 0, 4);
-    assert(i.size() == 4);
+    {
+        const unsigned char bytes[] = {'M','Z',0,1,2,3};
+        auto a = sleela::decompiler::Artifact::from_bytes(bytes, "sample.exe");
+        assert(a.format() == sleela::decompiler::Format::PE);
+        assert(a.size() == 6);
+        auto i = sleela::decompiler::Decoder{}.decode(a, 0, 4);
+        assert(i.size() == 4);
+    }
+    {
+        std::vector<std::uint8_t> ar{'!','<','a','r','c','h','>','\n'};
+        std::string h(60, ' ');
+        h.replace(0, 16, "member.o/");
+        h.replace(48, 10, "2");
+        h[58] = char(96);
+        h[59] = '\n';
+        ar.insert(ar.end(), h.begin(), h.end());
+        ar.insert(ar.end(), {0x7f, 'E', 'L', 'F'});
+        auto a = sleela::decompiler::Artifact::from_bytes(ar, "libsample.a");
+        assert(a.format() == sleela::decompiler::Format::GNUArchive);
+        assert(a.artifact_class() == sleela::decompiler::ArtifactClass::StaticArchive);
+        assert(a.interfaces().archive_members.size() == 1);
+        assert(a.interfaces().archive_members[0].name == "member.o");
+        assert(a.interfaces().archive_members[0].format == sleela::decompiler::Format::ELF);
+    }
     return 0;
 }
